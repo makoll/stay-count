fc2c2c3d3095bf5d0ec8c1d9bd3bb82e8c8209de
Makoto Hirose <makolllmako@gmail.com>
2021-06-16 22:57:51 +0900

    feat: introduce sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '~/styles/globals.css'
 
+import * as Sentry from '@sentry/react'
 import type { AppProps } from 'next/app'
+
+const DSN = 'https://7dc9a4361a1d4a2da97d414c4bc0470d@o855798.ingest.sentry.io/5819897'
+Sentry.init({
+  dsn: DSN,
+  tracesSampleRate: 1.0
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
